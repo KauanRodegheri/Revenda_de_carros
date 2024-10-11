740423ca7655905b6ad1899c618b2397602ba47e
Kauan Rodegheri <kauan.rodegheri@gmail.com>
2024-10-11 16:40:42 -0300

    teste

--- gemini_api/client.py
+++ gemini_api/client.py
@@ -4,7 +4,7 @@ import os
 def get_car_ai_bio(model, brand, year):
     prompt = f'''me mostre uma descrição de venda para o carro {brand} {model} {year} em apenas 
     200 caracteres. Descreva especificações técnicas desse modelo de carro.'''
-    genai.configure(api_key=os.environ["API_GEMINI"])
+    genai.configure(api_key='AIzaSyBJhRYbzuKa_MtpqHMXS2drqt12o4Y6cRU')
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(prompt)
     return response.text
