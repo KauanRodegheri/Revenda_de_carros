0980eade657c0d2d1ad2e9383b355ef9eb5134e2
Kauan Rodegheri <kauan.rodegheri@gmail.com>
2024-10-11 17:04:27 -0300

    resolvendo divergencias

--- gemini_api/client.py
+++ gemini_api/client.py
@@ -4,7 +4,11 @@ import os
 def get_car_ai_bio(model, brand, year):
     prompt = f'''me mostre uma descrição de venda para o carro {brand} {model} {year} em apenas 
     200 caracteres. Descreva especificações técnicas desse modelo de carro.'''
+<<<<<<< HEAD
     genai.configure(api_key=os.environ['API_GEMINI'])
+=======
+    genai.configure(api_key=os.environ["API_GEMINI"])
+>>>>>>> parent of 740423c (teste)
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(prompt)
     return response.text
